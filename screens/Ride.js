9c07b375b3673f3ad6762893cf33b574f46421d8
mrleafaro <91629924+mrleafaro@users.noreply.github.com>
2022-12-22 17:52:50 -0300

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -174,7 +174,7 @@ export default class RideScreen extends Component {
           // se a bicicleta estiver disponível, o tipo de transação será "rented",
           // caso contrário, será "return"
 
-          //transactionType = doc.data().is_bike_available ? "rented" : "return";
+          transactionType = doc.data().is_bike_available ? "rented" : "return";
           //transactionType = doc.data().is_bike_available ? "rented" ? "return";
           //transactionType === doc.data().is_bike_available ? "rented" : "return";
           //transactionType = doc.data().is_bike_available ? "rented" "return";
